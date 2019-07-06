7bf7a25a8534fde4ea5ea4789218d60f9cdb4416
James Rudd <james.rudd45@gmail.com>
2019-07-06 19:07:05 +1000

    final Commit, sorted Javascript into an object and updated references. cleaned up CSS

--- assets/javascript/updatedGame.js
+++ assets/javascript/updatedGame.js
@@ -2,104 +2,97 @@
 
 var game = {
     wordArray: ['borg', 'transporter', 'dilithium', 'lightsaber', 'force', 'starship', 'enterprise', 'alien', 'android', 'blaster', 'multiverse', 'universe', 'robot', 'spaceship', 'terraforming', 'tardis', 'hyperdrive', 'jedi', 'skywalker', 'federation', 'starfleet'],
-    chosenWord,
+    chosenWord: "",
     letterArray: [],
     dashArray: [],
     guessedLetters: [],
-    gameActive: false,
+    active: false,
     chances: 10,
     wins: 0,
     losses: 0,
+
+    chooseWord: function () {
+        game.dashArray = [];
+        game.chosenWord = game.wordArray[Math.floor(Math.random() * game.wordArray.length)];
+        game.letterArray = Array.from(game.chosenWord);
+        for (i = 0; i < game.letterArray.length; i++) {
+            game.dashArray.push("_");
+        };
+        document.getElementById('dashes').textContent = game.dashArray.join(' ');
+        game.active = true;
+    },
+
     restart: function () {
-        game.dashArray = [];
-        game.dashArray = [];
-        gameActive = false;
-        document.getElementById('guesses').textContent = "Guessed Letters: " + guessedLetters;
+        game.guessedLetters = [];
+        game.active = false;
+        document.getElementById('guesses').textContent = "Guessed Letters: " + game.guessedLetters;
         chances = 10;
     },
 
     update: function () {
-        document.getElementById('wins') = 'Wins: ' + game.wins;
-        document.getElementById('losses') = 'Losses: ' + game.losses;
-        document.getElementById('chances') = 'Remaining Chances: ' + game.chances;
-        document.getElementById('guesses') = 'Guessed Letters: ' + guessedLetters;
+        document.getElementById('wins').textContent = 'Wins: ' + game.wins;
+        document.getElementById('losses').textContent = 'Losses: ' + game.losses;
+        document.getElementById('chances').textContent = 'Remaining Chances: ' + game.chances;
+        document.getElementById('guesses').textContent = 'Guessed Letters: ' + game.guessedLetters;
+        document.getElementById('dashes').textContent = game.dashArray.join(' ');
     }
 }
 
-var remainingChances = document.getElementById("chances");
-var winTotal = document.getElementById("wins");
-var lossesTotal = document.getElementById("losses");
-var guesses = document.getElementById("guesses");
+
 
 document.onkeyup = function (event) {
-    keyPress = event.key;
-    keyCode = event.keyCode;
-    console.log(keyCode);
+    var keyPress = event.key;
+    var keyCode = event.keyCode;
 
-
-    if (keyPress === "Enter" && gameActive === false) {
-
-        //if there is no game in process it kicks off the startGame function
-        //It then changes the gameActive to true to prevent this function running again till the game is finished
-        var message = document.getElementById("test");
-        message.textContent = "Game Start!";
+    // If game.active is false (eg: no current game in progress) then we will
+    // Update our document banner to display the Game start message
+    // Update the main display sections to current scores and blank arrays
+    // choose a word and push that to the dash array
+    if (keyPress === "Enter" && game.active === false) {
+        document.getElementById('bannerMessage').textContent = "Game Start!";
         game.update();
-        gameActive = true;
-
-        // Choose a word from the array of possible choices
-        chosenWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-
-        // Then we take the chosenWord and convert to an array of all letters
-        letterArray = Array.from(chosenWord);
-
-        // convert the letter array to an array of dashes
-        for (i = 0; i < letterArray.length; i++) {
-            dashArray.push("_");
-        }
-
-        // display those dashes
-        dashes.textContent = dashArray.join(' ');
+        game.chooseWord();
     }
 
 
     // PLAY AREA
     //Game Playing
-    if (event.keyCode > 64 && event.keyCode < 91 && gameActive === true) {
-        if (guessedLetters.indexOf(keyPress.toUpperCase()) === -1) {
-            if (letterArray.indexOf(keyPress.toLowerCase()) !== -1) {
+    if (event.keyCode > 64 && event.keyCode < 91 && game.active === true) {
+
+        if (game.guessedLetters.indexOf(keyPress.toUpperCase()) === -1) {
+
+            if (game.letterArray.indexOf(keyPress.toLowerCase()) !== -1) {
                 // check the letterArray to see if the letter is in the array
-                for (i = 0; i < letterArray.length; i++) {
-                    if (letterArray[i] === keyPress.toLowerCase()) {
-                        dashArray[i] = keyPress.toUpperCase();
-                        dashes.textContent = dashArray.join(' ');
-                        guessedLetters.push(keyPress.toUpperCase());
-                        game.update;
+                for (i = 0; i < game.letterArray.length; i++) {
+                    if (game.letterArray[i] === keyPress.toLowerCase()) {
+                        game.dashArray[i] = keyPress.toUpperCase();
+                        dashes.textContent = game.dashArray.join(' ');
+                        game.guessedLetters.push(keyPress.toUpperCase());
+                        game.update();
                     }
                 }
             } else {
                 // if letter is not in the letterArray then it will reduce the remaining chances by one
-                chances = chances - 1;
+                game.chances = game.chances - 1;
                 game.update();
-                guessedLetters.push(keyPress.toUpperCase());
+                game.guessedLetters.push(keyPress.toUpperCase());
 
-                guesses.textContent = "Guessed Letters: " + guessedLetters.join(" ");
+                guesses.textContent = "Guessed Letters: " + game.guessedLetters.join(" ");
             }
         }
     }
 
     // Win/Loss scenarios
-    if ((dashArray.indexOf("_") === -1) && (gameActive === true)) {
-        var message = document.getElementById("test");
-        message.textContent = "You Win! Press ENTER to start again!"
-        wins = wins + 1;
+    if ((game.dashArray.indexOf("_") === -1) && (game.active === true)) {
+        document.getElementById("bannerMessage").textContent = "You Win! Press ENTER to start again!";
+        game.wins = game.wins + 1;
         game.restart();
         game.update();
     }
 
-    else if ((chances === 0) && (gameActive === true)) {
-        var message = document.getElementById("test");
-        message.textContent = "You LOSE! Press ENTER to try again."
-        losses = losses + 1;
+    else if ((game.chances === 0) && (game.active === true)) {
+        document.getElementById("bannerMessage").textContent = "You LOSE! Press ENTER to try again.";
+        game.losses = game.losses + 1;
         game.restart();
         game.update();
     }
